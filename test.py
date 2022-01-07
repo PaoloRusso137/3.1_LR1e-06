f8bb089ad13416ad68ae5d868b60f8da9758eb1f
PaoloRusso137 <95961400+PaoloRusso137@users.noreply.github.com>
2022-01-07 17:36:05 +0100

    Add files via upload

--- test.py
+++ test.py
@@ -1,59 +1,45 @@
-
-import faiss
+import math
 import torch
 import logging
 import numpy as np
 from tqdm import tqdm
-from torch.utils.data import DataLoader
-from torch.utils.data.dataset import Subset
-
-
-def test(args, eval_ds, model):
-    """Compute features of the given dataset and compute the recalls."""
-    model = model.eval()
-    with torch.no_grad():
-        logging.debug("Extracting database features for evaluation/testing")
-        # For database use "hard_resize", although it usually has no effect because database images have same resolution
-        database_subset_ds = Subset(eval_ds, list(range(eval_ds.database_num)))
-        database_dataloader = DataLoader(dataset=database_subset_ds, num_workers=args.num_workers,
-                                        batch_size=args.infer_batch_size, pin_memory=(args.device=="cuda"))
-        
-        all_features = np.empty((len(eval_ds), args.features_dim), dtype="float32")
-        for inputs, indices in tqdm(database_dataloader, ncols=100):
-            features = model(inputs.to(args.device))
-            features = features.cpu().numpy()
-            all_features[indices.numpy(), :] = features
-        
-        logging.debug("Extracting queries features for evaluation/testing")
-        queries_subset_ds = Subset(eval_ds, list(range(eval_ds.database_num, eval_ds.database_num+eval_ds.queries_num)))
-        queries_dataloader = DataLoader(dataset=queries_subset_ds, num_workers=args.num_workers,
-                                        batch_size=args.infer_batch_size, pin_memory=(args.device=="cuda"))
-        for inputs, indices in tqdm(queries_dataloader, ncols=100):
-            features = model(inputs.to(args.device))
-            features = features.cpu().numpy()
-            all_features[indices.numpy(), :] = features
-    
-    queries_features = all_features[eval_ds.database_num:]
-    database_features = all_features[:eval_ds.database_num]
-    
-    faiss_index = faiss.IndexFlatL2(args.features_dim)
-    faiss_index.add(database_features)
-    del database_features, all_features
-    
-    logging.debug("Calculating recalls")
-    distances, predictions = faiss_index.search(queries_features, max(args.recall_values))
-    
-    #### For each query, check if the predictions are correct
-    positives_per_query = eval_ds.get_positives()
-    # args.recall_values by default is [1, 5, 10, 20]
-    recalls = np.zeros(len(args.recall_values))
-    for query_index, pred in enumerate(predictions):
-        for i, n in enumerate(args.recall_values):
-            if np.any(np.in1d(pred[:n], positives_per_query[query_index])):
-                recalls[i:] += 1
-                break
-    # Divide by the number of queries*100, so the recalls are in percentages
-    recalls = recalls / eval_ds.queries_num * 100
-    recalls_str = ", ".join([f"R@{val}: {rec:.1f}" for val, rec in zip(args.recall_values, recalls)])
-    return recalls, recalls_str
-
+import torch.nn as nn
+import multiprocessing
+from os.path import join
+from datetime import datetime
+from torch.utils.data.dataloader import DataLoader
+torch.backends.cudnn.benchmark= True  # Provides a speedup
+ 
+import util
+import test
+import parser
+import commons
+import network
+import datasets_ws
+ 
+#### Initial setup: parser, logging...
+args = parser.parse_arguments()
+start_time = datetime.now()
+args.output_folder = join("runs", args.exp_name, start_time.strftime('%Y-%m-%d_%H-%M-%S'))
+commons.setup_logging(args.output_folder)
+commons.make_deterministic(args.seed)
+logging.info(f"Arguments: {args}")
+logging.info(f"The outputs are being saved in {args.output_folder}")
+logging.info(f"Using {torch.cuda.device_count()} GPUs and {multiprocessing.cpu_count()} CPUs")
+ 
+#### Creation of Datasets
+logging.debug(f"Loading dataset Pitts30k from folder {args.datasets_folder}")
+ 
+test_ds = datasets_ws.BaseDataset(args, args.datasets_folder, "pitts30k", "test")
+logging.info(f"Test set: {test_ds}")
+ 
+#### Initialize model
+model = network.GeoLocalizationNet(args)
+model = model.to(args.device)
+ 
+#### Test best model on test set
+best_model_state_dict = torch.load("/content/drive/MyDrive/Project_DAAI/runs/default/2022-01-05_16-06-14/best_model.pth")["model_state_dict"]
+model.load_state_dict(best_model_state_dict)
+ 
+recalls, recalls_str = test.test(args, test_ds, model)
+logging.info(f"Recalls on {test_ds}: {recalls_str}")
